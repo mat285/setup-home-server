d0436cb8889d546574198882e390c885e37e8c72
mat285 <mat285@cornell.edu>
2025-06-14 19:50:07 -0400

    simple

--- main.go
+++ main.go
@@ -1,21 +1,76 @@
 package main
 
 import (
-	"context"
 	"fmt"
+	"os"
+	"os/exec"
+	"sync"
+)
 
-	setuptasks "github.com/mat285/setup-home-server/setup-tasks"
-	"github.com/mat285/setup-home-server/task"
+var (
+	machines = []string{
+		"node0",
+		"node1",
+		"node2",
+		"node3",
+		"node4",
+		"node5",
+		"node6",
+		"node7",
+		"worker0",
+		"zstation",
+	}
+
+	version = "v0.2.0" // Update this to the latest version of your script
+
+	commands = []string{
+		`sudo sh -c "groupadd admin"`,
+		`sudo sh -c "usermod -aG admin michael"`,
+		`sudo sh -c "echo >> /etc/sudoers && echo 'michael ALL=(ALL) NOPASSWD: ALL' >> /etc/sudoers"`,
+		`sudo sh -c "apt install -y zsh"`,
+		`sudo sh -c "apt install -y btop iperf iperf3"`,
+		`sudo sh -c "snap install --classic go"`,
+		`sudo sh -c "snap install --classic kubectl"`,
+		`sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"`,
+		`sh -c "curl -fsSL https://tailscale.com/install.sh | sh"`,
+		`sh -c "$(curl -fsSL https://github.com/mat285/linklan/releases/download/` + version + `/install.sh)"`,
+	}
 )
 
 func main() {
-	runner, err := task.NewRunner(setuptasks.All()...)
-	if err != nil {
-		panic(err)
+	wg := &sync.WaitGroup{}
+	lock := &sync.Mutex{}
+	for _, machine := range machines {
+		wg.Add(1)
+		func(machine string) {
+			defer wg.Done()
+			lock.Lock()
+			fmt.Println("Setting up machine:", machine)
+			lock.Unlock()
+			for _, cmdStr := range commands {
+				lock.Lock()
+				fmt.Println("Running cmd:", cmdStr, "for machine:", machine)
+				lock.Unlock()
+				cmd := exec.Command("ssh",
+					"-A",
+					machine,
+					"-T",
+					cmdStr,
+				)
+				cmd.Env = append(os.Environ(), `SUDO_OPTS="-S"`)
+				output, err := cmd.CombinedOutput()
+				lock.Lock()
+				fmt.Println(string(output))
+				lock.Unlock()
+				if err != nil {
+					fmt.Printf("Error running command on %s: %v\n", machine, err)
+					continue
+				}
+			}
+			lock.Lock()
+			fmt.Printf("Successfully updated %s\n", machine)
+			lock.Unlock()
+		}(machine)
 	}
-	success, err := runner.Run(context.Background())
-	if err != nil {
-		panic(err)
-	}
-	fmt.Println("Command succesful:", success)
+	wg.Wait()
 }
